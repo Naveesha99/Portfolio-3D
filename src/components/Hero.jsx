a2f0ee1b2dbc2111b6356f48579d11f8ebcbd843
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2025-02-04 20:02:03 +0530

    header and sub header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { motion } from "framer-motion"
+
+import { styles } from "../styles"
+import { ComputersCanvas } from "./canvas"
+import { style } from "framer-motion/client"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Naveesha</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          I specialize in full-stack development, focusing on 
+          <br className="sm:block hidden" />MERN, Spring Boot, and Laravel, with a passion for UI/UX design.
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
