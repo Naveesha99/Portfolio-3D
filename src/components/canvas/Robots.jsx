9d1d470535ade8fed95dfcda878774d9856963bd
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2025-02-08 20:53:42 +0530

    hero section with 3d model

--- src/components/canvas/Robots.jsx
+++ src/components/canvas/Robots.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Robots = () => {
+const Robots = ({ isMobile }) => {
   const robot = useGLTF("./robot/scene.gltf");
   return (
     <mesh>
@@ -12,8 +12,8 @@ const Robots = () => {
       <pointLight intensity={1} />
       <primitive 
       object={robot.scene}
-      scale={4}
-      position={[0, -4, -1.5]} 
+      scale={isMobile ? 2.5 : 4}
+      position={isMobile? [0, -3.5, -2] : [0, -4, -1.5]} 
       rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -21,6 +21,21 @@ const Robots = () => {
 };
 
 const RobotCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    }
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => mediaQuery.removeEventListener("change", handleMediaQueryChange);
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
@@ -34,7 +49,7 @@ const RobotCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Robots />
+        <Robots isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
